af1423a08a627e7da3094dee0afef3f087088f29
Rochelle Castillo <castillorochelle27@gmail.com>
2025-10-29 12:12:19 +0800

    Title of Signup Changed

--- signup.php
+++ signup.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Sign Up</title>
     <?php include'Resources/includes.php';?>
     <link rel="stylesheet" href="Resources/style.css">
 </head>
